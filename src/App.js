fc4fcf70216ba8573c605cabcca6011e65b86144
Vinurachan <vinura.18@itfac.mrt.ac.lk>
2021-09-27 04:50:03 +0530

    routes edited

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 //import Footer from "./shared/Footer";
-import Timetable from "./dashboard/Timetable";
 import Navbar from "./shared/Navbar";
-
 import Homepage from "./home/Homepage";
-import { Route } from "react-router";
 import StudentView from "./student/StudentView";
 import Timetable from "./dashboard/Timetable";
-import StudentView from "./student/StudentView";
-
 
 const App = () => {
   return (
@@ -18,20 +13,14 @@ const App = () => {
       <Navbar />
       <Switch>
         <Route exact path="/">
-
           <Homepage />
-        </Route>
-
-        <Route exact path="/student">
-          <StudentView />
         </Route>
         <Route exact path="/dashboard">
           <Timetable />
         </Route>
-    <Route exact path="/student-view">
+        <Route exact path="/student-view">
           <StudentView />
         </Route>
-
       </Switch>
       {/* <Footer /> */}
     </>
