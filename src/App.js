a8f3a36b638a13387f2fa538410d89f8cd55f975
Nimasha Bandara <nimashak98@gmail.com>
2021-09-26 11:49:43 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./shared/Navbar";
+import Homepage from "./home/Homepage";
+import { Route } from "react-router";
+import StudentView from "./student/StudentView";
+import Timetable from "./dashboard/Timetable";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Switch>
-        {/* <Route exact path="/">
+        <Route exact path="/">
           <Homepage />
-        </Route> */}
+        </Route>
+
+        <Route exact path="/student">
+          <StudentView />
+        </Route>
+        <Route exact path="/dashboard">
+          <Timetable />
+        </Route>
       </Switch>
     </>
   );
