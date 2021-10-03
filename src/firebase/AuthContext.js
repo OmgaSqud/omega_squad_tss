1a871a8da3b6e3bcc7f86b54ffb91cc0f765569a
ManishyamAbeyweera <manishyam.rc@gmail.com>
2021-10-03 21:30:10 +0530

    join buttons added

--- src/firebase/AuthContext.js
+++ src/firebase/AuthContext.js
@@ -1,14 +1,19 @@
 import React, { createContext, useEffect, useState } from "react";
 import { db, auth } from "./Firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { useHistory } from "react-router";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = (props) => {
   const [user, setUser] = useState("INIT");
+  const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged(async (fbUser) => {
+      if (fbUser == null) {
+        return history.push("/");
+      }
       const docRef = doc(db, "users", fbUser.uid);
       const docSnap = await getDoc(docRef);
 
