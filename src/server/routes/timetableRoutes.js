a6b46f7d08bcb1b721873aa80296f27cf2ad5f5b
Vinurachan <vinura.18@itfac.mrt.ac.lk>
2022-01-16 21:40:26 +0530

    added backend hosting URL,  edited package.JSON

--- src/server/routes/timetableRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const timetable = require("../controllers/timetable");
-
-router.route("/newMeeting").post(timetable.createZoomLink);
-router.route("/deletePeriod").post(timetable.notifyDelete);
-
-module.exports = router;
